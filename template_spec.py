65207294003260fc04a67c6fa8c5b4031c5d7eb2
Ty K <tykavanaugh@gmail.com>
2021-10-08 08:14:50 -0500

    Create template_spec.py

---
+++ template_spec.py
@@ -0,0 +1,27 @@
+from sum_pairs import sum_pairs
+import unittest
+
+
+class ChangeMeTests(unittest.TestCase):
+    def test_test_something(self):
+        """Text"""
+        self.assertEqual(function(args) , desired_return)
+
+# Common Unit Tests:
+# assertEqual(a, b) > a == b
+# assertNotEqual(a, b) > a != b
+# assertTrue(x) > bool(x) is True
+# assertFalse(x) > bool(x) is False
+# assertIs(a, b) > a is b
+# assertIsNot(a, b) > a is not b
+# assertIsNone(x) > x is None
+# assertIsNotNone(x) > x is not None
+# assertIn(a, b) > a in b
+# assertNotIn(a, b) > a not in b
+# assertIsInstance(a, b) > isinstance(a, b)
+# assertNotIsInstance(a, b) > not isinstance(a, b)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
